a02446cdf385cb99b2e068e967f717c4fc12c7af
ZacharyMaeshima <ZMaeshima@gmail.com>
2021-04-04 22:10:51 -0500

    Reorganize files. Implement routing for the 3 pages

--- app.py
+++ app.py
@@ -4,15 +4,21 @@ import BarcodeScanner
 app = Flask(__name__)
 
 
-@app.route("/", methods = ["POST", "GET"])
-def home:
+@app.route("/", methods = ["GET", "POST"])
+def home():
     return render_template("index.html")
 
 
-@app.route("/scan", methods = ["POST", "GET"])
+@app.route("/scan", methods = ["GET","POST"])
 def scan():
     return render_template("grid-barcode.html")
 
 @app.route("/getbarcode", methods = ["GET"])
 def getBarcode():
     return BarcodeScanner.UPC_lookup(BarcodeScanner.barcode_reader())
+
+
+@app.route("/checkout", methods = ["GET", "POST"])
+def checkout():
+    return render_template("exit.html")
+
